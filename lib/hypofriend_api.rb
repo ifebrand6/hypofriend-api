bf7fa4b4e2ae867b03607846f7e7bcc29bec745b
ifebrand6 <ifebrand6@gmail.com>
2021-10-09 19:06:04 +0100

    first commit

---
+++ lib/hypofriend_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hypofriend_api/version"
+
+module HypofriendApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hypofriend_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HypofriendApi
+  VERSION = "0.1.0"
+end
