1512f044a8f2d9c3d004544c9a0c5ffaf6846867
ifebrand6 <ifebrand6@gmail.com>
2021-10-11 00:28:16 +0100

    gem scaffold

--- lib/hypofriend_api.rb
+++ lib/hypofriend_api.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
-
+require 'net/http'
 require_relative "hypofriend_api/version"
+require 'hypofriend_api/food'
 
 module HypofriendApi
   class Error < StandardError; end
   # Your code goes here...
+  # fetch the api and display it as ob
+  class RequestAPI
+    uri = URI('https://offer-v3.hypofriend.de/api/v5/new-offers?loan_amount=350000&property_value=350000&repayment=1.0&years_fixed=15')
+    Net::HTTP.get(uri)
+    def getOffer
+      res = Net::HTTP.get_response(uri)
+      return res
+    end
+  end
+ 
 end
---
+++ lib/hypofriend_api/objects/base.rb
@@ -0,0 +1,11 @@
+class HypofriendBaseObject
+    require 'json'
+    attr_reader :hypofriend_api
+    
+    def initialize(hypofriend_object)
+
+    #   @hypofriend_object = object
+    end
+
+
+end
