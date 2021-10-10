f5c52c649bb9aa1d91be91cbe90ac3e687b277f5
ifebrand6 <ifebrand6@gmail.com>
2021-10-11 00:29:28 +0100

    add API module for cleaner code

---
+++ lib/hypofriend_api/modules/api.rb
@@ -0,0 +1,8 @@
+module API
+	BASE_URL = "https://offer-v3.hypofriend.de/api/v5/"
+	NEW_OFFER_PATH = "/new-offers"
+	PROPERTY_VALUE = "property_value"
+	LOAN_VALUE = "loan-value"
+	REPAYMENT = "repayment"
+	YEARS_FIXED = "years_fixed"
+end
